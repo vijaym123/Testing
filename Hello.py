402fe6b49360098775a813d5ced51301775d9050
Vijay Mahantesh SM <vijay@vijay.(none)>
2012-03-19 21:40:56 +0530

    Hello World Program

---
+++ Hello.py
@@ -0,0 +1,9 @@
+#Creator : Vijay Mahantesh SM
+#Date : March 19th 2012
+
+import random
+
+print random.random()
+#Generates random number between 0 to 1
+
+print "Hello World ! This is my first program to be uploaded in Git server."
